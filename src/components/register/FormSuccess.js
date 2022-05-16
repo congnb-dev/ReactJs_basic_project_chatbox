1192f6142c3063758db1938ebf9d7c76cbb08bc5
Bao Cong <ngobaocong.it@gmail.com>
2022-05-16 19:49:19 +0700

    commit source

---
+++ src/components/register/FormSuccess.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Form.css';
+import aaa from './img/th.jpg'
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>Chúng tôi đã nhận được yêu cầu của bạn!</h1>
+      <img className="thanhcong" src={aaa} alt="Italian Trulli"></img>
+     
+    </div>
+  );
+};
+
+export default FormSuccess;
